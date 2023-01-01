ef91d6d240fcbc3025bb575fcbb66ec7dc73cec7
orcheink <861640471@qq.com>
2023-01-01 23:15:27 +0800

    docs(ranger): add profile

---
+++ .config/ranger/commands.py
@@ -0,0 +1,354 @@
+
+from __future__ import (absolute_import, division, print_function)
+from ranger.api.commands import Command
+from ranger.core.loader import CommandLoader
+import os
+
+import re
+import curses
+import collections
+import subprocess
+
+
+# ALT-M fzf mark
+class fzf_mark(Command):
+    """
+    `:fzf_mark` refer from `:fzf_select`  (But Just in `Current directory and Not Recursion`)
+        so just `find` is enough instead of `fdfind`)
+
+    `:fzf_mark` can One/Multi/All Selected & Marked files of current dir that filterd by `fzf extended-search mode` 
+        fzf extended-search mode: https://github.com/junegunn/fzf#search-syntax
+        eg:    py    'py    .py    ^he    py$    !py    !^py
+    In addition:
+        there is a delay in using `get_executables` (So I didn't use it)
+        so there is no compatible alias.
+        but find is builtin command, so you just consider your `fzf` name
+    Usage
+        :fzf_mark
+        
+        shortcut in fzf_mark:
+            <CTRL-a>      : select all 
+            <CTRL-e>      : deselect all 
+            <TAB>         : multiple select
+            <SHIFT+TAB>   : reverse multiple select
+            ...           : and some remap <Alt-key> for movement
+    """
+
+    def execute(self):
+
+        fzf_name = "fzf" 
+
+        hidden = ('-false' if self.fm.settings.show_hidden else r"-path '*/\.*' -prune")
+        exclude = r"\( -name '\.git' -o -iname '\.*py[co]' -o -fstype 'dev' -o -fstype 'proc' \) -prune"
+        only_directories = ('-type d' if self.quantifier else '')
+        fzf_default_command = 'find -L . -mindepth 1 -type d -prune {} -o {} -o {} -print | cut -b3-'.format(
+            hidden, exclude, only_directories
+        )
+
+        env = os.environ.copy()
+        env['FZF_DEFAULT_COMMAND'] = fzf_default_command
+
+        # you can remap and config your fzf (and your can still use ctrl+n / ctrl+p ...) + preview
+        env['FZF_DEFAULT_OPTS'] = '\
+        --multi \
+        --reverse \
+        --bind ctrl-a:select-all,ctrl-e:deselect-all,alt-n:down,alt-p:up,alt-o:backward-delete-char,alt-h:beginning-of-line,alt-l:end-of-line,alt-j:backward-char,alt-k:forward-char,alt-b:backward-word,alt-f:forward-word \
+        --height 100% \
+        --layout reverse \
+        --border \
+        --preview-window=up \
+        --color "fg:#0078d4,bg:#212121,preview-fg:#e8e28d,preview-bg:#3b4254,pointer:#fff100" \
+        --preview "highlight -O ansi -l {1} || bat --style=numbers --color=always --line-range :500 {}"'
+
+        fzf = self.fm.execute_command(fzf_name, env=env, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
+
+        if fzf.returncode == 0:
+            filename_list = stdout.strip().split()
+            for filename in filename_list:
+                # Python3.4+
+                # self.fm.select_file( str(Path(filename).resolve()) )
+                self.fm.select_file( os.path.abspath(filename) )
+                self.fm.mark_files(all=False,toggle=True)
+
+
+
+# CTRL-F fzf search
+class fzf_select(Command):
+
+    def execute(self):
+        from ranger.ext.get_executables import get_executables
+
+        if 'fzf' not in get_executables():
+            self.fm.notify('Could not find fzf in the PATH.', bad=True)
+            return
+
+        fd = None
+        if 'fdfind' in get_executables():
+            fd = 'fdfind'
+        elif 'fd' in get_executables():
+            fd = 'fd'
+
+        if fd is not None:
+            hidden = ('--hidden' if self.fm.settings.show_hidden else '')
+            exclude = "--no-ignore-vcs --exclude '.git' --exclude '*.py[co]' --exclude '__pycache__'"
+            only_directories = ('--type directory' if self.quantifier else '')
+            fzf_default_command = '{} --follow {} {} {} --color=always'.format(
+                fd, hidden, exclude, only_directories
+            )
+        else:
+            hidden = ('-false' if self.fm.settings.show_hidden else r"-path '*/\.*' -prune")
+            exclude = r"\( -name '\.git' -o -iname '\.*py[co]' -o -fstype 'dev' -o -fstype 'proc' \) -prune"
+            only_directories = ('-type d' if self.quantifier else '')
+            fzf_default_command = 'find -L . -mindepth 1 {} -o {} -o {} -print | cut -b3-'.format(
+                hidden, exclude, only_directories
+            )
+
+        env = os.environ.copy()
+        env['FZF_DEFAULT_COMMAND'] = fzf_default_command
+        env['FZF_DEFAULT_OPTS'] = '--color "fg:#0078d4,bg:#212121,preview-fg:#e8e28d,preview-bg:#3b4254,pointer:#fff100" --preview-window=up --border --height=100% --layout=reverse --ansi --preview="{}"'.format('''
+            (   
+                highlight -O ansi -l {1} ||
+                bat --color=always {} ||
+            ) 2>/dev/null | head -n 100
+        ''')
+
+        fzf = self.fm.execute_command('fzf --no-multi', env=env,
+                                      universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
+        if fzf.returncode == 0:
+            selected = os.path.abspath(stdout.strip())
+            if os.path.isdir(selected):
+                self.fm.cd(selected)
+            else:
+                self.fm.select_file(selected)
+
+
+
+# sshfs_mount && sshfs_umount
+# usage: sshfs_mount user@host:target
+
+def show_error_in_console(msg, fm):
+    fm.notify(msg, bad=True)
+
+def navigate_path(fm, selected):
+    if not selected:
+        return
+
+    selected = os.path.abspath(selected)
+    if os.path.isdir(selected):
+        fm.cd(selected)
+    elif os.path.isfile(selected):
+        fm.select_file(selected)
+    else:
+        show_error_in_console(f"Neither directory nor file: {selected}", fm)
+        return
+
+def execute(cmd, input=None):
+    stdin = None
+    if input:
+        stdin = subprocess.PIPE
+
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=stdin, text=True)
+    stdout, stderr = proc.communicate(input=input)
+
+    if proc.returncode != 0:
+        raise Exception(f"Bad process exit code: {proc.returncode}, stdout={stdout}, stderr={stderr}")
+
+    return stdout, stderr
+
+URL = collections.namedtuple("URL", ["user", "hostname", "path"])
+
+def parse_url(url):
+    if len(t := url.split(sep="@", maxsplit=1)) > 1:
+        user = t[0]
+        rest = t[1]
+    else:
+        user = None
+        rest = t[0]
+    if len(t := rest.rsplit(sep=":", maxsplit=1)) > 1:
+        hostname = t[0]
+        path = t[1]
+    else:
+        hostname = t[0]
+        path = None
+
+    return URL(user=user, hostname=hostname, path=path)
+
+def url2str(u :URL):
+    res = u.hostname
+    if u.user:
+        res = f"{u.user}@{res}"
+    path = u.path
+    if path is None:
+        path = ""
+
+    return f"{res}:{path}"
+
+def search_mount_path(mount_path):
+    stdout, _ = execute(["mount"])
+    return re.search(re.escape(mount_path)+r"\b", stdout)
+
+def hostname2mount_path(hostname):
+    mount_path = os.path.expanduser(f"~/.config/ranger/mounts/{hostname}")
+
+    # check whether it is already mounted
+    if search_mount_path(mount_path):
+        raise Exception(f"Already mounted: {mount_path}")
+
+    os.makedirs(mount_path, exist_ok=True)
+    return mount_path
+
+class sshfs_mount(Command):
+    def execute(self):
+        url = self.arg(1)
+        u = parse_url(url)
+
+        mount_path = hostname2mount_path(u.hostname)
+        cmd = ["sshfs", url2str(u), mount_path]
+
+        execute(cmd)
+
+        # before navigating we should load it otherwise we see
+        # "not accessible"
+        d = self.fm.get_directory(mount_path)
+        d.load()
+
+        navigate_path(self.fm, mount_path)
+
+    # options:
+    # - None
+    # - string: just one complete without iterating
+    # - list, tuple, generator: to iterate options around
+    def tab(self, tabnum):
+        u = parse_url(self.rest(1))
+
+        def path_options():
+            lst = []
+            for path in ["", "/"]:
+                lst.append(self.start(1) + url2str(u._replace(path=path)))
+
+            return lst
+
+        # autocomplete hostname
+        if u.path is None:
+            hostname = select_with_fzf(["fzf", "-q", u.hostname], compose_hostname_list(), self.fm)
+            #hostname = "ilya-thinkpad"
+
+            # after suspend/init we should manually show the cursor
+            # the same way console.open() does
+            try:
+                curses.curs_set(1)
+            except curses.error:
+                pass
+
+            if not hostname:
+                return None
+
+            u = u._replace(hostname=hostname)
+            return path_options()
+
+        # autocomplete path
+        return path_options()
+
+def umount(mount_path):
+    prefix = os.path.expanduser(f"~/.config/ranger/mounts/")
+    if not mount_path.startswith(prefix):
+        raise Exception(f"May umount only inside: {prefix}")
+
+    if not search_mount_path(mount_path):
+        raise Exception(f"Not mounted: {mount_path}")
+
+    cmd = ["umount", mount_path]
+    execute(cmd)
+    os.rmdir(mount_path)
+
+class sshfs_umount(Command):
+    def execute(self):
+        tab = self.fm.tabs[self.fm.current_tab]
+        mount_path = tab.thisfile.path
+        umount(mount_path)
+
+def compose_hostname_list():
+    # list of possible hostnames
+    # stolen from fzf, https://github.com/junegunn/fzf/blob/master/shell/completion.bash
+    stdout, _ = execute(["bash"], input='''
+command cat <(
+    command tail -n +1 ~/.ssh/config ~/.ssh/config.d/* /etc/ssh/ssh_config 2> /dev/null | command grep -i '^\s*host\(name\)\? ' | awk '{for (i = 2; i <= NF; i++) print $1 " " $i}' | command grep -v '[*?]') \
+        <(command grep -oE '^[[a-z0-9.,:-]+' ~/.ssh/known_hosts | tr ',' '\n' | tr -d '[' | awk '{ print $1 " " $1 }') \
+        <(command grep -v '^\s*\(#\|$\)' /etc/hosts | command grep -Fv '0.0.0.0') |
+        awk '{if (length($2) > 0) {print $2}}' | sort -u
+''')
+    return stdout
+
+
+
+# mkdir && cd
+class mcd(Command):
+    """
+    :mkcd <dirname>
+
+    Creates a directory with the name <dirname> and enters it.
+    """
+
+    def execute(self):
+        from os.path import join, expanduser, lexists
+        from os import makedirs
+        
+        dirname = join(self.fm.thisdir.path, expanduser(self.rest(1)))
+        if not lexists(dirname):
+            makedirs(dirname)
+
+            match = re.search('^/|^~[^/]*/', dirname)
+            if match:
+                self.fm.cd(match.group(0))
+                dirname = dirname[match.end(0):]
+
+            for m in re.finditer('[^/]+', dirname):
+                s = m.group(0)
+                if s == '..' or (s.startswith('.') and not self.fm.settings['show_hidden']):
+                    self.fm.cd(s)
+                else:
+                    ## We force ranger to load content before calling `scout`.
+                    self.fm.thisdir.load_content(schedule=False)
+                    self.fm.execute_console('scout -ae ^{}$'.format(s))
+        else:
+            self.fm.notify("file/directory exists!", bad=True)
+
+
+
+# quickly uploading a file to a server via scp.
+# usage: up user@host:target
+class up(Command):
+    def execute(self):
+        if self.arg(1):
+            scpcmd = ["scp", "-r"]
+            scpcmd.extend([f.realpath for f in self.fm.thistab.get_selection()])
+            scpcmd.append(self.arg(1))
+            self.fm.execute_command(scpcmd)
+            self.fm.notify("Uploaded!")
+
+    def tab(self, tabnum):
+        try:
+            import paramiko
+        except ImportError:
+            """paramiko not installed"""
+            return
+
+        try:
+            with open(os.path.expanduser("~/.ssh/config")) as file:
+                paraconf = paramiko.SSHConfig()
+                paraconf.parse(file)
+        except IOError:
+            """cant open ssh config"""
+            return
+
+        hosts = sorted(list(paraconf.get_hostnames()))
+        # remove any wildcard host settings since they're not real servers
+        hosts.remove("*")
+        query = self.arg(1) or ''
+        matching_hosts = []
+        for host in hosts:
+            if host.startswith(query):
+                matching_hosts.append(host)
+        return (self.start(1) + host + ":" for host in matching_hosts)
