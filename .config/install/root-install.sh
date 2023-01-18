3b40abe5b8f6d768c0c6b1cd6bcd892ad2df6545
orcheink <861640471@qq.com>
2023-01-18 21:32:54 +0800

    build(install): update install.sh

--- .config/install/root-install.sh
+++ .config/install/root-install.sh
@@ -10,14 +10,12 @@ sudo rm /usr/share/zsh/site-functions/_trash*
 git config --system user.name orcheink
 git config --system user.email 861640471@qq.com
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/OrcheInk/.arch-profile/main/.config/install/sync.sh)"
-
 cat $HOME/.config/npm/npmrc | sudo tee /etc/npmrc
 npm install -g tldr
 rm /usr/lib/node_modules/tldr/config.json
 cat $HOME/.config/tldr/config.json | sudo tee /usr/lib/node_modules/tldr/config.json
 
-cat $HOME/config/zsh/zshenv | sudo tee /etc/zsh/zshenv
+cat $HOME/.config/zsh/zshenv | sudo tee /etc/zsh/zshenv
 
 # finish
 echo "The installation is complete !"
--- .config/install/sync.sh
+++ .config/install/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ssh-keyscan github.com >> $HOME/.ssh/known_hosts
-git clone --bare https://github.com/OrcheInk/.arch-profile.git $HOME/.arch-profile
+git clone --bare git@github.com:OrcheInk/.arch-profile.git $HOME/.arch-profile
 
 # alias for arhc-profile
 function arch {
