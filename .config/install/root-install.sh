b00f7c4ca40710efd5a9ba80f04e43fa29b6747f
orcheink <861640471@qq.com>
2023-01-28 20:43:52 +0800

    build(install): update install.sh

--- .config/install/root-install.sh
+++ .config/install/root-install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-sudo pacman -Sy
-sudo pacman --noconfirm -S openssh git neovim zsh fzf fd ripgrep bat highlight ranger sshfs unzip unrar p7zip trash-cli python-pip tree most lua man-db nodejs npm github-cli
+pacman -Sy
+pacman --noconfirm -S openssh git neovim zsh fzf fd ripgrep bat highlight ranger sshfs unzip unrar p7zip trash-cli python-pip tree most lua man-db nodejs npm github-cli
 
 # trash-cli completion
 pip install 'trash-cli[completion]'
-sudo rm /usr/share/zsh/site-functions/_trash*
+rm /usr/share/zsh/site-functions/_trash*
 
 git config --system user.name orcheink
 git config --system user.email 861640471@qq.com
+git config --system --add url."git@github.com:".insteadOf "https://github.com/"
 
 cat $HOME/.config/npm/npmrc | sudo tee /etc/npmrc
 npm install -g tldr
